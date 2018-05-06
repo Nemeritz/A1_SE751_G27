22d888ba8fa3426421e400b57316e29e4430fbac
Jerry <nano@pixelcollider.net>
2018-05-06 13:12:15 +1200

    ByteArray utility class to store byte[] in list

---
+++ src/main/java/facegallery/utils/ByteArray.java
@@ -0,0 +1,13 @@
+package facegallery.utils;
+
+public class ByteArray {
+	private final byte[] bytes;
+	
+	public ByteArray(byte[] bytes) {
+		this.bytes = bytes;
+	}
+	
+	public byte[] getBytes() {
+		return bytes;
+	}
+}
