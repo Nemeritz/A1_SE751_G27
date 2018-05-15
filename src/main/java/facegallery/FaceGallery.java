96db1472e74592aceedd4733fbe3cf0ccfb43237
Jerry <nano@pixelcollider.net>
2018-05-15 19:29:20 +1200

    Never mind about pipelining, attempting to fix bug

--- src/main/java/facegallery/FaceGallery.java
+++ src/main/java/facegallery/FaceGallery.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class FaceGallery {
-    public static String DATASET_DIR = System.getenv("FACEGALLRY_DATASET");
+    public static String DATASET_DIR = "C:\\Users\\lichk\\Documents\\Git\\facedataset";
 
 	public static void main(String[] args) {
 	    try (Scanner scanner = new Scanner(System.in)) {
@@ -51,20 +51,22 @@ public class FaceGallery {
 	}
 
 	public static void runSequential() {
-        ImageBytesReader imageBytesReader = new ImageBytesReader(DATASET_DIR + File.pathSeparator + "Test");
+        ImageBytesReader imageBytesReader = new ImageBytesReader(DATASET_DIR + File.separator + "Test");
+        List<File> fileList = List.of(imageBytesReader.getFileList());
         List<ByteArray> fileBytes = imageBytesReader.runSequential();
 
         FaceDetector faceDetector = new FaceDetector(fileBytes);
         List<AtomicBoolean> detections = faceDetector.runSequential();
 
         System.out.println("Sequential as follows:");
-        for (AtomicBoolean detection : detections) {
-            System.out.println(detection.toString());
+        for (int i = 0; i < detections.size(); i++) {
+            System.out.println(fileList.get(i).getName() + ": " + detections.get(i).toString());
         }
     }
 
     public static void runParallel() {
-        ImageBytesReader imageBytesReader = new ImageBytesReader(DATASET_DIR + File.pathSeparator + "Test");
+        ImageBytesReader imageBytesReader = new ImageBytesReader(DATASET_DIR + File.separator + "Test");
+        List<File> fileList = List.of(imageBytesReader.getFileList());
         List<ByteArray> fileBytes = imageBytesReader.createResultsContainer();
         List<AtomicBoolean> bytesReady = imageBytesReader.createReadyContainer();
 
@@ -76,25 +78,23 @@ public class FaceGallery {
 
         if (bytesRead) {
             boolean detectionsDone = faceDetector.runParallel(detections, detectionsReady);
-
             if (detectionsDone) {
                 System.out.println("Parallel as follows:");
-                for (AtomicBoolean detection : detections) {
-                    System.out.println(detection.toString());
+                for (int i = 0; i < detections.size(); i++) {
+                    System.out.println(fileList.get(i).getName() + ": " + detections.get(i).toString());
                 }
             }
         }
     }
 
     public static void runParallelPipeline() {
-        ImageBytesReader imageBytesReader = new ImageBytesReader(DATASET_DIR + File.pathSeparator + "Test");
+        ImageBytesReader imageBytesReader = new ImageBytesReader(DATASET_DIR + File.separator + "Test");
         List<ByteArray> fileBytes = imageBytesReader.createResultsContainer();
         List<AtomicBoolean> bytesReady = imageBytesReader.createReadyContainer();
 
         FaceDetector faceDetector = new FaceDetector(fileBytes);
         List<AtomicBoolean> detections = faceDetector.createResultsContainer();
         List<AtomicBoolean> detectionsReady = faceDetector.createReadyContainer();
-
     }
 
     public static void runGui() {
--- src/main/java/facegallery/utils/Parallelized.java
+++ src/main/java/facegallery/utils/Parallelized.java
@@ -1,16 +1,12 @@
 package facegallery.utils;
 
-import org.jetbrains.annotations.Nullable;
-
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public abstract class Parallelized<I, R> {
     protected List<I> inputs;
     protected ConcurrentLinkedQueue<Integer> processed = new ConcurrentLinkedQueue<>();
 
-    @Nullable
     protected Parallelized(List<I> inputs) {
         if (inputs != null) {
             this.inputs = inputs;
@@ -23,9 +19,7 @@ public abstract class Parallelized<I, R> {
 
     public abstract List<R> runSequential();
 
-    public abstract boolean runParallel(List<R> results, List<AtomicBoolean> ready);
+    public abstract boolean runParallel(List<R> results);
 
     public abstract List<R> createResultsContainer();
-
-    public abstract List<AtomicBoolean> createReadyContainer();
 }
